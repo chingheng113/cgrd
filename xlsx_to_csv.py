c76ee446b770ede75264463939c51f634ab113e8
Ching-Heng <chingheng113@gmail.com>
2019-08-17 17:02:57 -0400

    bk

--- xlsx_to_csv.py
+++ xlsx_to_csv.py
@@ -5,11 +5,12 @@ current_path = os.path.dirname(__file__)
 
 def xlsx_to_csv_pd():
     read_path = glob.glob(os.path.join(current_path, 'xls', '*.xlsx'))
-    for p in  read_path:
+    for p in read_path:
         output_filename = str(os.path.basename(p)).replace('.xlsx', '')
-        data_xls = pd.read_excel(p, index_col=0, encoding='utf-8')
-        data_xls.to_csv(os.path.join('csv', output_filename+'.csv'), encoding='utf_8_sig')
-        print(output_filename)
+        if not os.path.isfile(os.path.join('csv', output_filename+'.csv')):
+            data_xls = pd.read_excel(p, index_col=0, encoding='utf-8')
+            data_xls.to_csv(os.path.join('csv', output_filename+'.csv'), encoding='utf_8_sig')
+            print(output_filename)
 
 
 if __name__ == '__main__':
