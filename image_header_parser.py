6349ac66048167916a1a8fe5d7ffd11e6f6a3ea9
Ching-Heng <chingheng113@gmail.com>
2019-12-23 16:00:15 -0500

    bk

--- image_header_parser.py
+++ image_header_parser.py
@@ -10,7 +10,7 @@ import csv
 
 dicom_keywords = ['ImageType', 'PatientID', 'PatientBirthDate', 'PatientSex', 'StudyDate', 'SeriesDate', 'PatientWeight',
                   'ContentDate', 'StudyTime', 'SeriesTime', 'ContentTime', 'AccessionNumber', 'RequestAttributesSequence',
-                  'PerformedProcedureStepID', 'requestid', 'StudyID', 'Modality', 'InstanceNumber']
+                  'PerformedProcedureStepID', 'requestid', 'StudyID', 'Modality', 'Manufacturer']
 
 
 root_dir = '//nindsdirfs//shares//BIS//Amar//Stroke_Shared_Folder//2019_07_02_CGRD_Stroke/Images//'
@@ -45,4 +45,17 @@ with open('dicom_header.csv', mode='w', newline='') as dicom_header_file:
                                 head_info_list.append(elem.value)
                         csv_writer.writerow(head_info_list)
         print(patients_dir)
+
+
+# By folloing code, we find out that '1.2.528.1.1001.200.10.4573.10409.3754721344.20190926025013594' and
+# '1.2.528.1.1001.200.10.4573.10409.3754721344.20190926021755157' are bad data.
+# so the number of patient is 7148, not 7150
+# df = pd.read_csv('dicom_header.csv')
+# dir = df['dir'].str.split('\\').str[0].str.split('//').str[-1]
+# uniq = list(dict.fromkeys(dir))
+# print(len(uniq))
+# patients_dirs = os.listdir(root_dir)
+# uniq_p = list(dict.fromkeys(patients_dirs))
+# print(len(uniq_p))
+# print(list(set(uniq_p)-set(uniq)))
 print('done')
